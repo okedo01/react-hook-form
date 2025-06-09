ae4c2e643a052b3a102b913a50a3395365c9a358
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-09 15:47:59 +0300

    Adding javascript for validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
-  const []
+  const [ error, setError ] = useState("");
+  const [ isSubmitting, setIsSubmitting ] = useState(false);
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("")
+  const [ confirmPassword, setConfirmPassword ] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    if(password !== confirmPassword) {
+      console.log("mismatch");
+      
+    }
+  }
+
   return (
     <>
       <h1>React Hook Form</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="email" placeholder='Enter email'/>
         <input type="password" placeholder='Enter password'/>
         <input type="password" placeholder='Confirm password'/>
-        <button>Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     </>
   )
