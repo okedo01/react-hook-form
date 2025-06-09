49b83e55ede54a7605302ba6fd0bc6fc4b613641
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-09 15:59:28 +0300

    Adding useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,25 @@ function App() {
   const [ password, setPassword ] = useState("")
   const [ confirmPassword, setConfirmPassword ] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    isSubmitting(true);
     
     if(password !== confirmPassword) {
       console.log("mismatch");
-      
     }
+
+    isSubmitting(false);
   }
 
   return (
     <>
       <h1>React Hook Form</h1>
       <form onSubmit={handleSubmit}>
-        <input type="email" placeholder='Enter email'/>
-        <input type="password" placeholder='Enter password'/>
-        <input type="password" placeholder='Confirm password'/>
-        <button type='submit'>Submit</button>
+        <input type="email" placeholder='Enter email' value={email} onChange={() => setEmail(e.target.value)}/>
+        <input type="password" placeholder='Enter password'value={password} onChange={() => setPassword(e.target.value)}/>
+        <input type="password" placeholder='Confirm password'value={confirmPassword} onChange={() => setConfirmPassword(e.target.value)}/>
+        <button type='submit' disabled={isSubmitting}>Submit</button>
       </form>
     </>
   )
