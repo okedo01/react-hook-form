3d878ebb274ca269c58487bf7da7e1fda2b4612e
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-09 16:39:27 +0300

    Submit the form using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
 
-  const [ error, setError ] = useState("");
+  const [ errors, setErrors ] = useState([]);
   const [ isSubmitting, setIsSubmitting ] = useState(false);
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("")
@@ -14,7 +14,7 @@ function App() {
     setIsSubmitting(true);
     
     if(password !== confirmPassword) {
-      setError("Passwords mismatch");
+      setErrors(["Passwords mismatch"]);
       setIsSubmitting(false);
       return;
     }
@@ -22,13 +22,23 @@ function App() {
     await new Promise(resolve => setTimeout(resolve, 1000));
 
     setIsSubmitting(false);
+    setEmail("")
+    setPassword("");
+    setConfirmPassword("");
+    setErrors([]);
   }
 
   return (
     <>
       <h1>React Hook Form</h1>
-      <p className='error'>{error}</p>
       <form onSubmit={handleSubmit}>
+        { errors.length > 0 && (
+          <ul>
+            { errors.map(error => (
+              <li key={error} className='error'>{ errors }</li>
+            ))}
+          </ul>
+        )}
         <input type="email" placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}/>
         <input type="password" placeholder='Enter password'value={password} onChange={(e) => setPassword(e.target.value)}/>
         <input type="password" placeholder='Confirm password'value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
